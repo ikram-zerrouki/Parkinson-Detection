09968442726bed19368209cbdad2d737bcb1e526
ikram-zerrouki <ki_zerrouki@esi.dz>
2025-03-04 14:55:12 +0100

    new commit

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Home from '../components/Home';  // One level up to access the components directory
+
+
+function HomeP () {
+  return (
+    
+  );
+};
+
+export default HomeP;
